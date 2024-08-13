b1f92d3025ca654826440f1f943298654064da9f
Aliaksandr Kicka <aliaksandr.kicka@gmail.com>
2024-08-13 10:50:34 +0200

    fix: changing index.js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let i = Math.abs(n);
+    let reversed = i.toString().split('').reverse().join('');
+    return Number(reversed); 
+};
+
+
+/*
+1.  Преобразуем число в положительное
+2.  Переворачиваем строку
+    split - разбивает объект String на массив
+    reverse - переворачивает порядок следования элементов массива.
+    join -  объединяет все элементы массива
+3.  Возвращаем перевернутое число
+*/
